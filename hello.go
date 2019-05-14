e491a2f0bc5fd8a11972a8af2079467bdda1cda6
huangqingbing 00442524 <huangqingbing1@huawei.com>
2019-05-14 16:25:16 +0800

    fix typo

--- hello.go
+++ hello.go
@@ -1,8 +1,8 @@
 package testgomod
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 )
 
 // Say hello
@@ -10,7 +10,7 @@ func Hello(who, lang string) (string, error) {
 	switch lang {
 	case "en":
 		return fmt.Sprintf("Hello, %s!", who), nil
-	cse "cn":
+	case "cn":
 		return fmt.Sprintf("你好, %s!", who), nil
 	default:
 		return "", errors.New("unknown language")

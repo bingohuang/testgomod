d74759e540f633fdc34e0e30d2ae86b131b908ce
huangqingbing 00442524 <huangqingbing1@huawei.com>
2019-05-14 16:15:23 +0800

    change func Hello

--- hello.go
+++ hello.go
@@ -1,8 +1,18 @@
 package testgomod
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // Say hello
-func Hello(who string) string {
-	return fmt.Sprintf("Hello, %s!", who)
+func Hello(who, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hello, %s!", who), nil
+	cse "cn":
+		return fmt.Sprintf("你好, %s!", who), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
